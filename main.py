bbc448d6bdf55e3e4f6e8a1cea30c0bb345a87fe
Thomas Ryan <thomryan@cisco.com>
2023-08-03 11:23:39 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import json
+
+from fastapi import FastAPI, Request
+from fastapi.responses import FileResponse
+
+app = FastAPI()
+
+@app.get("/")
+async def root():
+    return FileResponse("index.html")
+
+@app.get("/config")
+async def get_config():
+    return FileResponse("static/config.json")
+
+@app.post("/config")
+async def set_config(request: Request):
+    with open("static/config.json", "w") as f:
+        json.dump(await request.json(), f)
+        return True
+    return False
+
+@app.get("/base_config")
+async def base_config():
+    return FileResponse("static/base_config.json")
